1f7f5ea604f22c24aba69b029bd2b0fa1f885a4a
Uriel Quiroz <uriel.quiroz187@gmail.com>
2021-07-28 16:57:32 -0500

    Calculo del seguro
    Se creo el prototype cotizarSeguro para calcular el seguro

--- js/app.js
+++ js/app.js
@@ -5,6 +5,51 @@ function Seguro(marca, year, tipo) {
     this.tipo = tipo;
 }
 
+//Realiza la cotizacion con los dotos
+Seguro.prototype.cotizarSeguro = function() {
+    /*
+        1 = Americano 1.15
+        2 = Asiatico 1.05
+        3 = Europeo 1.35
+     */
+
+        let cantidad;
+        const base = 2000;
+
+        switch (this.marca) {
+            case '1':
+                cantidad = base * 1.15;
+                break;
+            case '2':
+                cantidad = base * 1.05;
+                break;
+            case '3':
+                cantidad = base * 1.35;
+                break;
+        
+            default:
+                break;
+        }
+
+        //Leer el año
+        const diferencia = new Date().getFullYear() - this.year;
+
+        //Cada alo que la diferencia es mayor, el costo va a reducirse un 3%
+        cantidad -= ((diferencia * 3) * cantidad) / 100;
+
+        /**
+         * Si el seguro es básico se multiplica por un 30% mas
+         * Si el seguro es completo se multiplica por un 50% mas
+         */
+        if (this.tipo === 'basico') {
+            cantidad *= 1.30;
+        } else {
+            cantidad *= 1.50;
+        }
+        
+        return cantidad;
+}
+
 function UI() {}
 
 //Llena las opciones de los años
@@ -75,4 +120,9 @@ function cotizarSeguro(e){
     }
 
     ui.mostrarMensaje('Cotizando...', 'exito');
+
+
+    //Instanciamos
+    const seguro = new Seguro(marca, year, tipo);
+    seguro.cotizarSeguro();
 }
